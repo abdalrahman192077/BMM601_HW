459c8133bc5346a11323b604a5d9e31e2d7b81f0
abdalrahman192077 <167198225+abdalrahman192077@users.noreply.github.com>
2025-10-25 21:52:41 +0300

    upload assets

---
+++ assets/js/main.js
@@ -0,0 +1,272 @@
+let lang_json = `{
+  "translations1_en": [
+    "Home",
+    "Color patterns",
+    "Multimedia",
+    "Introduction to Multimedia. In addition to learning about color patterns.",
+    "Show More",
+    "What is The Multimedia?",
+    "For computer seller",
+    "For a computer seller, multimedia is a set of components and technical capabilities that enable the playback of various content (audio, video, images, etc.), such as a computer with speakers, a microphone, and a DVD or CD player.",
+    "Ordinary consumer",
+    "For the average consumer, multimedia might be thought of as high-speed internet, television, or any other electronic device.",
+    "ITE Students",
+    "For ITE students and the technical community, multimedia is viewed as applications that include text, images, graphics, motion, video, and audio. It is therefore closely related to many fields of study, such as computer vision, information compression, networking, and databases.",
+    "Known multimedia applications",
+    "There are many popular multimedia applications that are becoming more and more relied upon over time.",
+		"Multimedia software tools",
+		"Audio software",
+		"Image software",
+		"Image software",
+		"Video software",
+		"Animation software",
+		"Animation software",
+		"Animation software",
+		"Animation software"
+  ],
+  "translations1_ar": [
+    "الرئيسية",
+    "انماط الألوان",
+    "الوسائط المتعددة",
+		"مقدمة في الوسائط المتعددة. بالإضافة إلى التعرف على أنماط الألوان.",
+		"عرض المزيد",
+		"ما هي الوسائط المتعددة؟",
+		"بالنسبة لبائع الحواسيب",
+		"بالنسبة لبائع الحواسيب، تُعد الوسائط المتعددة مجموعة من المكونات والقدرات التقنية التي تتيح تشغيل أنواع مختلفة من المحتوى (الصوت، الفيديو، الصور، إلخ)، مثل الحاسوب المزود بسماعات، ميكروفون، ومشغل أقراص DVD أو CD.",
+		"بالنسبة للمستهلك العادي",
+		"بالنسبة للمستهلك العادي، قد يُنظر إلى الوسائط المتعددة على أنها الإنترنت عالي السرعة، التلفاز، أو أي جهاز إلكتروني آخر.",
+		"طلاب هندسة المعلوماتية",
+		"بالنسبة لطلاب هندسة المعلوماتية والمجتمع التقني، تُعتبر الوسائط المتعددة تطبيقات تحتوي على نصوص، صور، رسومات، حركة، فيديو، وصوت. لذا فهي مرتبطة ارتباطاً وثيقاً بالعديد من مجالات الدراسة مثل الرؤية الحاسوبية، ضغط المعلومات، الشبكات، وقواعد البيانات.",
+		"تطبيقات الوسائط المتعددة المعروفة",
+		"هناك العديد من تطبيقات الوسائط المتعددة الشائعة التي يُعتمد عليها بشكل متزايد مع مرور الوقت.",
+		"الادوات البرمجية للوسائط المتعددة",
+		"برمجيات للصوت",
+		"برمجيات للصور",
+		"برمجيات للصور",
+		"برمجيات للفيديو",
+		"برمجيات للتحريك",
+		"برمجيات للتحريك",
+		"برمجيات للتحريك",
+		"برمجيات للتحريك"
+  ],
+  "translations2_en": [
+    "Home",
+    "Color patterns",
+    "Color patterns",
+    "Colors... The language of life that speaks without words! Learn digital color patterns!",
+    "Digital image processing extracts data from the color spectrum!",
+    "In multimedia research, we will learn about color patterns, video, and audio, and how to process them!",
+    "We'll learn color patterns for digital photos and videos!",
+    "On the last slide you can experiment with mixing colors!",
+    "RGB color mode",
+    "The image is stored in 3 color components: R, G, B, and each component is represented by 8 bits.",
+    "The three colors are mixed after being balanced differently according to the following relationship:",
+    "CMY and CMYK color mode",
+    "When two light beams fall on an object, the resulting light is their sum.",
+		"However, the situation is different when printing on paper, where the primary colors become Yellow (Y), Magenta (M), and Cyan (C).",
+		"Mixing C with M and Y gives a brownish-black color, so it is more economical to add the black color compound K.",
+		"YUV color mode for digital video",
+		"The Y component indicates the luminance, while the U and V components indicate the color difference according to the equation",
+		"We notice that we can get YUV in terms of RGB.",
+		"Apply what you've learned using the color tool!",
+		"Click on the white box to start.",
+		"Colors are keys",
+		"Remember that every color carries a message. May your next messages be filled with positivity and creativity."
+  ],
+  "translations2_ar": [
+		"الرئيسية",
+		"أنماط الألوان",
+		"أنماط الألوان",
+		"الألوان... لغة الحياة التي تتحدث دون كلمات! تعلّم أنماط الألوان الرقمية!",
+		"معالجة الصور الرقمية تستخرج البيانات من طيف الألوان!",
+		"في بحث الوسائط المتعددة، سنتعلم عن أنماط الألوان، الفيديو، والصوت، وكيفية معالجتها!",
+		"سنتعلم أنماط الألوان للصور والفيديوهات الرقمية!",
+		"في الشريحة الأخيرة يمكنك تجربة مزج الألوان!",
+		"نمط ألوان RGB",
+		"تُخزن الصورة في 3 مكونات لونية: R، G، B، وكل مكون يُمثّل بـ 8 بت.",
+		"تُخلط الألوان الثلاثة بعد موازنتها بشكل مختلف وفق العلاقة التالية:",
+		"نمط ألوان CMY و CMYK",
+		"عندما تسقط شعاعا ضوء على جسم، يكون الضوء الناتج هو مجموعهما.",
+		"لكن الوضع يختلف عند الطباعة على الورق، حيث تصبح الألوان الأساسية هي الأصفر (Y)، الأرجواني (M)، والسماوي (C).",
+		"مزج C مع M و Y يعطي لوناً أسود مائل للبني، لذا من الأكثر اقتصاداً إضافة مركب اللون الأسود K.",
+		"نمط ألوان YUV للفيديو الرقمي",
+		"المكون Y يدل على الإضاءة، بينما المكونان U و V يدلان على فرق اللون وفق المعادلة.",
+		"نلاحظ أنه يمكننا الحصول على YUV من خلال RGB.",
+		"طبّق ما تعلمته باستخدام أداة الألوان!",
+		"انقر على المربع الأبيض للبدء.",
+		"الألوان هي مفاتيح",
+		"تذكّر أن كل لون يحمل رسالة. نتمنى أن تكون رسائلك القادمة مليئة بالإيجابية والإبداع"
+  ]
+}`
+
+const changeBootstrapCDN = (ln)=>{
+	const BS_LinkFather = document.getElementById('b-s-cdn')
+	const BS_Link = document.querySelector('#b-s-cdn link')
+	if(BS_Link){
+		BS_Link.remove();
+		if(ln == 'ar'){
+			BS_LinkFather.innerHTML =`
+				<link  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.rtl.min.css" integrity="sha384-Xbg45MqvDIk1e563NLpGEulpX6AvL404DP+/iCgW9eFa2BqztiwTexswJo2jLMue" crossorigin="anonymous">
+			`
+			
+		}else{
+			BS_LinkFather.innerHTML =`
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css"
+				integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=="
+				crossorigin="anonymous"
+				/>
+			`
+		}
+	}
+} 
+
+const changeDir = (dir)=>{
+	document.documentElement.setAttribute('dir', dir);
+	dir == 'rtl' ? document.documentElement.classList.add('rtl-dir') : document.documentElement.classList.remove('rtl-dir')
+
+	let swip_toolsExist = document.querySelector('.swip-tools')
+	if(swip_toolsExist){
+		let swip_tools = new Swiper('.swip-tools')
+		swip_tools.destroy(true, true);
+		
+		setTimeout(()=>{
+			swip_tools = new Swiper('.swip-tools', {
+				slidesPerView: 2,
+				spaceBetween: 0,
+				direction: 'horizontal',
+				rtl: document.documentElement.getAttribute('dir') == 'rtl' ? 'rtl' : 'ltr',
+				watchSlidesProgress: true,
+				watchOverflow: true,
+				loop: false,
+				breakpoints: {
+					1400: {
+						slidesPerView: 6
+					},
+					1200: {
+						slidesPerView: 5
+					},
+					991: {
+						slidesPerView: 4
+					},
+					576: {
+						slidesPerView: 3
+					},
+				},
+				
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true
+				},
+			});
+		}, 100)
+		
+	}else{
+		let swip_content = new Swiper('.swip-content')
+		swip_content.destroy(true, true)
+		
+		setTimeout(()=>{
+			swip_content = new Swiper('.swip-content', {
+				slidesPerView: 1,
+				spaceBetween: 0,
+				direction: 'horizontal',
+				rtl: document.documentElement.getAttribute('dir') == 'rtl' ? 'rtl' : 'ltr',
+				watchSlidesProgress: true,
+				watchOverflow: true,
+				loop: false,
+				
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true
+				},
+			});
+		}, 100)
+
+	}
+	
+}
+
+const changeTx = (ln)=>{
+	const allEleContainTx = document.querySelectorAll('.contain-text')
+	if(allEleContainTx.length > 0){
+		const pageIs = document.querySelector('#home-body')
+		let allTx = JSON.parse(lang_json) 
+	
+		if(pageIs){
+			let Texts
+			if(ln == 'en'){
+				Texts = allTx["translations1_en"]
+				allEleContainTx.forEach((ele, index)=>{
+					ele.textContent = Texts[index]
+				})
+				
+			}else{
+				Texts = allTx["translations1_ar"]
+				allEleContainTx.forEach((ele, index)=>{
+					ele.textContent = Texts[index]
+				})
+			}
+
+		}else{
+			let Texts
+			if(ln == 'en'){
+				Texts = allTx["translations2_en"]
+				allEleContainTx.forEach((ele, index)=>{
+					ele.textContent = Texts[index]
+				})
+				
+			}else{
+				Texts = allTx["translations2_ar"]
+				allEleContainTx.forEach((ele, index)=>{
+					ele.textContent = Texts[index]
+				})
+			}
+
+		}
+		
+	}
+}
+
+const lang_btn = document.querySelector('li.change-lang-btn');
+console.log(lang_btn)
+if(lang_btn){
+	lang_btn.addEventListener('click', ()=>{
+		if(localStorage.getItem('lang') == 'ar'){
+			localStorage.setItem('lang', 'en')
+			changeBootstrapCDN('en')
+			changeDir('ltr')
+			lang_btn.textContent = 'AR'
+			changeTx('en')
+			
+		}else if(localStorage.getItem('lang') == 'en'){
+			localStorage.setItem('lang', 'ar')
+			changeBootstrapCDN('ar')
+			changeDir('rtl')
+			lang_btn.textContent = 'EN'
+			changeTx('ar')
+			
+		}else{
+			localStorage.setItem('lang', 'en')
+			changeBootstrapCDN('en')
+			changeDir('ltr')
+			lang_btn.textContent = 'AR'
+			changeTx('en')
+		}
+	})
+}
+
+if(localStorage.getItem('lang')){
+	let lang = localStorage.getItem('lang')
+	if(lang == 'ar'){
+		changeBootstrapCDN('ar')
+		changeDir('rtl')
+		lang_btn.textContent = 'EN'
+		changeTx('ar')
+	}else{
+		changeBootstrapCDN('en')
+		changeDir('ltr')
+		lang_btn.textContent = 'AR'
+		changeTx('en')
+	}
+}else{
+	changeDir('rtl')
+}
